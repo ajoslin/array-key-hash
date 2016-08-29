a3ff2e8a1124256db55ca403fe1102c67020f4a6
Andrew Joslin <andrewtjoslin@gmail.com>
2016-08-28 20:30:43 -0600

    Use for loop

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
 
 module.exports = function arrayKeyHash (array, idKey) {
   idKey = idKey || 'id'
+  var result = {}
 
-  return array.reduce(function (acc, value) {
-    if (!value) return acc
+  for (var i = 0, length = array.length; i < length; i++) {
+    var item = array[i]
+    if (!item) continue
 
-    const id = value[idKey]
-    if (id != null) {
-      acc[id] = value
-    }
-    return acc
-  }, {})
+    var id = item[idKey]
+    if (id == null) continue
+
+    result[id] = item
+  }
+
+  return result
 }
